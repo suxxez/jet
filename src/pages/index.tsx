52076d13dcf2817ef38719a0095e6ef4660f0aa5
Finn Fentker <coding@fentker.com>
2021-03-24 13:52:08 +0100

    Change text and layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,21 +10,30 @@ import Card from "../components/card"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    
-    <h1 className="mainHeading">Jakobi Elektrotechnik</h1>
+    <div className="titlePlaceholder"></div>
+    <h1 className="mainHeading">
+      Ihr Ansprechpartner für Elektrotechnik im Münsterland.
+    </h1>
+    <p className="whiteParagraph">Lernen Sie unser Unternehmen kennen:</p>
+    {/* <div className="videoWrapper">
+    <iframe
+      width="355"
+      height="200"
+      src="https://www.youtube-nocookie.com/embed/cw602cSBfZg?controls=0"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullscreen
+    ></iframe>
+    </div> */}
     <video autoPlay loop muted src={VideoTest} id="introVideo"></video>
-    <div className="placeholder"></div>
     <div className="content">
       <div className="cards">
         <Card imageSrc="gatsby-astronaut.png" heading="Beratung"></Card>
         <Card imageSrc="gatsby-icon.png" heading="Planung"></Card>
         <Card imageSrc="gatsby-astronaut.png" heading="Installation"></Card>
       </div>
-      <i className="fas fa-comments"></i>
-
-
     </div>
-    
   </Layout>
 )
 
