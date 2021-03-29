12269de837ac3db5714792798056234741f209de
Finn Fentker <coding@fentker.com>
2021-03-30 01:20:05 +0200

    Fix transitions for other browsers, eager loading for hamburger

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -72,6 +72,7 @@ const Header = () => {
             <StaticImage
               src="../images/svg/hamburger.svg"
               alt="X"
+              loading="eager"
               className="hamburger"
             />
           </li>
