a2065f0168f8f520401b8ee5ccd836e6961971b3
Finn Fentker <coding@fentker.com>
2021-04-01 11:55:20 +0200

    Add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,13 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <h1>404: Nicht gefunden!</h1>
+    <p>Diese Seite existiert nicht. <Link to="/" style={{color: "#de354c"}}>Hier geht es zur Startseite zurück.</Link></p>
   </Layout>
 )
 
