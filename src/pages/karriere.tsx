00254d79d93455a0753989da3bdaa8d94d284c45
Finn Fentker <coding@fentker.com>
2021-04-02 17:46:24 +0200

    Add demo karriere page

---
+++ src/pages/karriere.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function Karriere() {
+  return (
+    <Layout>
+      <SEO title="Karriere" />
+      <div>
+        <h1>Karriere</h1>
+        <p>Aktuell sind keine Stellen ausgeschrieben.</p>
+      </div>
+    </Layout>
+  )
+}
